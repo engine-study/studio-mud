a601c4c799b6aaa8ef523112f388e29651f7b6cc
Neilson K-S <neilson.ks@gmail.com>
2023-07-16 12:05:55 -0700

    more txmanager functions

--- Runtime/Scripts/Core/TxManager.cs
+++ Runtime/Scripts/Core/TxManager.cs
@@ -10,53 +10,52 @@ using Nethereum.Contracts.CQS;
 using Nethereum.Contracts;
 using Cysharp.Threading.Tasks;
 
-namespace mud.Client
-{
+namespace mud.Client {
 
     public class TxManager : MonoBehaviour {
 
         public static System.Action<bool> OnTransaction;
-        public static async UniTask<bool> Send<TFunction>(params object[] parameters) where TFunction : FunctionMessage, new()
-        {
+
+        public static async UniTask<bool> Send<TFunction>(params object[] parameters) where TFunction : FunctionMessage, new() {
             bool txSuccess = await NetworkManager.Instance.worldSend.TxExecute<TFunction>(parameters);
-            CleanupTransaction(txSuccess);
-            return txSuccess;
-        }
-
-        //optimistically update something
-        public static async UniTask<bool> Send<TFunction>(List<TxUpdate> updates, params object[] parameters) where TFunction : FunctionMessage, new()
-        {
-            bool txSuccess = await NetworkManager.Instance.worldSend.TxExecute<TFunction>(parameters);
-
-            if(txSuccess)  {
-                Debug.Log("Success");
-            } else {
-                //if our transaction fails, force the player back to their position on the table
-                Debug.Log("Reverting");
-                foreach(TxUpdate u in updates) { u.Revert();}
-            }
-
-            CleanupTransaction(txSuccess);
-            return txSuccess;
-        }
-
-        private static void CleanupTransaction(bool txSuccess) {
             OnTransaction?.Invoke(txSuccess);
+            return txSuccess;
         }
 
+        //enables us to send transactions by inferring functionTyp through the parameter
+        public static async UniTask<bool> Send<TFunction>(TFunction functionType, params object[] parameters) where TFunction : FunctionMessage, new() {
+            return await Send<TFunction>(parameters);
+        }
+
+        
         public static TxUpdate MakeOptimistic(MUDComponent component, params object[] tableParameters) {
             //update the component
             TxUpdate update = new TxUpdate(component, tableParameters);
             return update;
         }
 
+        //send transaction but revert our optimistic updates if it goes wrong
+        public static async UniTask<bool> Send<TFunction>(List<TxUpdate> updates, params object[] parameters) where TFunction : FunctionMessage, new() {
+            
+            bool txSuccess = await Send<TFunction>(parameters);
+
+            if (txSuccess) {
+                Debug.Log("Success");
+            } else {
+                //if our transaction fails, force the player back to their position on the table
+                Debug.Log("Reverting");
+                foreach (TxUpdate u in updates) { u.Revert(); }
+            }
+
+            return txSuccess;
+        }
+
+
     }
 
     [System.Serializable]
-    public struct TxUpdate
-    {
-        public TxUpdate(MUDComponent c, params object[] tableParameters)
-        {
+    public struct TxUpdate {
+        public TxUpdate(MUDComponent c, params object[] tableParameters) {
             component = c;
 
             //derive table from component
