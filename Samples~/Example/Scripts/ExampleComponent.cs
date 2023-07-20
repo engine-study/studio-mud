067f5f3599f6cffe4e819229c831d96fabece4d5
Neilson K-S <neilson.ks@gmail.com>
2023-07-20 08:42:44 +0200

    duped samples folder

---
+++ Samples~/Example/Scripts/ExampleComponent.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace mud.Client {
+
+    public class ExampleComponent : MUDComponent
+    {
+        protected override void UpdateComponent(Client.IMudTable table, UpdateInfo newInfo) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Samples~/Example/Scripts/MUDDemo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace mud.Client
+{
+
+    public class MUDDemo : MonoBehaviour
+    {
+
+        void Start()
+        {   
+            Debug.LogError("Work In Progress!");
+            Debug.LogError("Contact @neilsonks for help.");
+        }
+
+    }
+    
+}
