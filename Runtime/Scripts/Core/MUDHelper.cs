c19d8915db65209c0e891a642ca70447fb536af9
Neilson K-S <neilson.ks@gmail.com>
2023-07-20 12:46:31 +0200

    Update MUDHelper.cs

--- Runtime/Scripts/Core/MUDHelper.cs
+++ Runtime/Scripts/Core/MUDHelper.cs
@@ -224,5 +224,14 @@ namespace mud.Client {
             }
             return abiValues;
         }
+
+        public static byte[] ToByteArray(string HexString) {
+            int NumberChars = HexString.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2) {
+                bytes[i / 2] = System.Convert.ToByte(HexString.Substring(i, 2), 16);
+            }
+            return bytes;
+        }
     }
 }
